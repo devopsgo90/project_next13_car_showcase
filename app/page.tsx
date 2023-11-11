e01bbc49ed3701b60c2e4912eb245c0768e2bb84
devopsgo90 <fachrihamzah53@gmail.com>
2023-11-11 16:40:21 +0700

    update sabtu

--- app/page.tsx
+++ app/page.tsx
@@ -20,8 +20,8 @@ export default async function Home({ searchParams }: HomeProps) {
 
       <div className='mt-12 padding-x padding-y max-width' id='discover'>
         <div className='home__text-container'>
-          <h1 className='text-4xl font-extrabold'>Car Catalogue</h1>
-          <p>Explore out cars you might like</p>
+          <h1 className='text-4xl font-extrabold'>Katalog Mobil</h1>
+          <p>Cari mobil yang kamu suka</p>
         </div>
 
         <div className='home__filters'>
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -17,17 +17,16 @@ const Hero = () => {
     <div className="hero">
       <div className="flex-1 pt-36 padding-x">
         <h1 className="hero__title">
-          Find, book, rent a car—quick and super easy!
+          Rental mobil favoritmu disini, sekarang !
           
         </h1>
 
         <p className="hero__subtitle">
-          Streamline your car rental experience with our effortless booking
-          process.
+        Sederhanakan pengalaman sewa mobil Anda dengan proses pemesanan kami yang mudah.
         </p>
 
         <CustomButton
-          title="Explore Cars"
+          title="Cari Mobil"
           containerStyles="bg-primary-blue text-white rounded-full mt-10"
           handleClick={handleScroll}
         />
